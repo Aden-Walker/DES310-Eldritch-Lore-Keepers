1d501194cd7b39251328e3ba7dea14095ea60352
Dominic Barrie <2002152@uad.ac.uk>
2023-02-21 14:13:43 +0000

    Made the Anteater Move
    More teleporting rather than moving but it is a solid start to the beginning of the movement system for the point and click adventure.

--- Anteaters/Assets/Scripts/MouseClicking.cs
+++ Anteaters/Assets/Scripts/MouseClicking.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class MouseClicking : MonoBehaviour
 {
-    public Transform clickPosition;
-    public Transform playerPosition;
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0))
         {
-            clickPosition.position = Input.mousePosition;
+
+            var hit = new RaycastHit();
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //transform.position = Input.mousePosition;
+
+            float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
+            transform.position = new Vector3(curPosition.x, Mathf.Max(0.5f, curPosition.y), transform.position.z);
+
+            //clickPosition.position = Input.mousePosition;
             //this gives a point that can be used as a component in other objects
         }
         
