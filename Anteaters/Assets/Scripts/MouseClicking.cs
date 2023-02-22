f84731235749b87ee112dc1ce4cdc384798ebd73
Dominic Barrie <2002152@uad.ac.uk>
2023-02-22 14:00:51 +0000

    Fixed Merge Conflicts
    Fixed the issue with Unity Merge Conflicts so that now the movement for the point and click works without errors on the main branch.

--- Anteaters/Assets/Scripts/MouseClicking.cs
+++ Anteaters/Assets/Scripts/MouseClicking.cs
@@ -6,18 +6,12 @@ public class MouseClicking : MonoBehaviour
 {
 
     Vector3 positionToMoveTo;
-
-<<<<<<< Updated upstream
-=======
     void Start()
     {
         
         positionToMoveTo = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
     }
-
->>>>>>> Stashed changes
-    // Update is called once per frame
     void Update() {
         
         if (Input.GetMouseButtonDown(0))
@@ -30,22 +24,13 @@ public class MouseClicking : MonoBehaviour
 
             float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
-<<<<<<< Updated upstream
-            positionToMoveTo = new Vector3(curPosition.x, Mathf.Max(0.5f, curPosition.y), transform.position.z);
-=======
             positionToMoveTo = new Vector3(curPosition.x, curPosition.y, transform.position.z);
->>>>>>> Stashed changes
 
             //clickPosition.position = Input.mousePosition;
             //this gives a point that can be used as a component in other objects
         }
-
-<<<<<<< Updated upstream
-            transform.position += transform.position - positionToMoveTo;
         
-=======
         transform.position = new Vector3((positionToMoveTo.x - transform.position.x) / 100 + transform.position.x, (positionToMoveTo.y - transform.position.y) / 100 + transform.position.y, transform.position.z);
->>>>>>> Stashed changes
         
     }
 }
