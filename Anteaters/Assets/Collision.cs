e5b084287bc49c39c22657c0b60eb2bccd65d3e6
Aden Walker <60225464+Aden-Walker@users.noreply.github.com>
2023-03-08 16:41:47 +0000

    Collision based text triggers correctly

--- Anteaters/Assets/Collision.cs
+++ Anteaters/Assets/Collision.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
+
+    public TextTrigger textTrigger;
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Collided");
-      
+        textTrigger.TriggerDialogue();
     }
 }
