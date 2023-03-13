d067d4e8dde6ee097a8d26b1204edeba4c3a2ce3
1901150 <60218980+1901150@users.noreply.github.com>
2023-03-13 20:51:15 +0000

    Realised parallax doesn't need to be that complicated
    added simple parallax script that can be expanded as needed

--- Anteaters/Assets/Scripts/ParallaxTracking.cs
+++ Anteaters/Assets/Scripts/ParallaxTracking.cs
@@ -4,35 +4,28 @@ using UnityEngine;
 
 public class ParallaxTracking : MonoBehaviour
 {
-    // generic tracking script seperate from the camera
+    // Simple parallax Script
 
-    public Transform trackedObject;
-    public float trackingSpeed;
-    public Vector2 trackingOffset;
-    public Vector2 boundLeft;
-    public Vector2 boundRight;
-    private Vector3 offset;
+    //variables to store shit. parallaxFactor should be a value between 0 and 1 higher values = less movement of the object
+    private float startPos;
+    public float parallaxFactor;
+    public Transform cam;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = (Vector3)trackingOffset;
-        offset.z = transform.position.z - trackedObject.position.z;
+        //store the start position of the object
+        startPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 modTracked = trackedObject.position;
-        // Check if out of bounds, if so return to bounds
-        if (modTracked.x < boundLeft.x)
-            modTracked.x = boundLeft.x;
-        if (modTracked.x > boundRight.x)
-            modTracked.x = boundRight.x;
-        if (modTracked.y > boundLeft.y)
-            modTracked.y = boundLeft.y;
-        // Move the object
-        transform.position = Vector3.MoveTowards(transform.position, modTracked + offset, trackingSpeed * Time.deltaTime);
+        //figure out the distance that the object needs to move and then move it
+        float distance = cam.position.x * parallaxFactor;
+        Vector3 newPosition = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        transform.position = newPosition;
+     
     }
 }
