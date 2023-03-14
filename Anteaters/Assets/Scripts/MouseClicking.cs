251f52fb6180febd38e43180cf3be6aae8e891ae
Dominic Barrie <2002152@uad.ac.uk>
2023-03-14 11:40:32 +0000

    Added in comments.
    Comments have been added into the code, do not worry about the deletions this is just due to me doing inline comments the code is all the same. Only ignore or modify changes if it has that stupid unity github stuff in it.

--- Anteaters/Assets/Scripts/MouseClicking.cs
+++ Anteaters/Assets/Scripts/MouseClicking.cs
@@ -6,15 +6,17 @@ public class MouseClicking : MonoBehaviour
 {
     //more or less the player class
 
+    //Animator for the Anteater
     Animator animator;
 
-    Vector3 positionToMoveTo;
-    float moveX, speed, conversionX, conversionY;
-    bool facingRight = true;
+    Vector3 positionToMoveTo; //The position that we hope to get to.
+    float moveX, speed, conversionX, conversionY; //A bunch of movement variables, moveX is how far we move in the horizontal direction, speed is how fast we go and the Conversion variables determine the rate at which speeds are divided.
+    bool facingRight = true; //A variable to check if the anteater is facing right or not. Used for Flipping.
     void Start()
     {
-        animator = GetComponent<Animator>();
-        positionToMoveTo = new Vector3(-4.25f, transform.position.y, transform.position.z);
+        //We are initializing all of our variables here.
+        animator = GetComponent<Animator>(); //Starts the animator? - Dominic
+        positionToMoveTo = new Vector3(-4.25f, transform.position.y, transform.position.z); //Initialize our two movement points to be the starting position to prevent null values.
         transform.position = new Vector3(-4.25f, transform.position.y, transform.position.z);
         moveX = 0.0f;
         speed = 20.0f;
@@ -24,28 +26,28 @@ public class MouseClicking : MonoBehaviour
     }
     void Update()
     {
-        bool moving = false;
+        bool moving = false; //We make sure that moving is false every frame until we determine it is true.
         if ((Input.GetMouseButtonDown(0)) && (Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(gameObject.transform.position).z)).y < 0))
         {
 
-            var hit = new RaycastHit();
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = new RaycastHit(); //A raycast for detection
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition); //A ray itself seeing where the mouse is.
 
             //transform.position = Input.mousePosition;
 
-            float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
-            positionToMoveTo = new Vector3(curPosition.x, curPosition.y, transform.position.z);
+            float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z; //How far away is the objects from the screen.
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen)); //The position we are at currently with our mouse
+            positionToMoveTo = new Vector3(curPosition.x, curPosition.y, transform.position.z); //Where are we going? Where are we off to?
             //clickPosition.position = Input.mousePosition;
             //this gives a point that can be used as a component in other objects
         }
 
-        if (positionToMoveTo.x - transform.position.x > 0.0f)
+        if (positionToMoveTo.x - transform.position.x > 0.0f) //Now we try to see if we are moving in the right direction.
         {
 
-            moveX = Mathf.Min((positionToMoveTo.x - transform.position.x) / conversionX + transform.position.x, speed / conversionX + transform.position.x);
+            moveX = Mathf.Min((positionToMoveTo.x - transform.position.x) / conversionX + transform.position.x, speed / conversionX + transform.position.x); //We move either the speed of the anteater or slowly go to center - Bad Code, am changing
 
-            if (!facingRight)
+            if (!facingRight) //This checks if we are facing left, if we are we will flip.
             {
 
                 Flip();
@@ -53,14 +55,14 @@ public class MouseClicking : MonoBehaviour
 
             }
 
-            moving = true;
+            moving = true; //I like to move it move it.
         }
-        else if (positionToMoveTo.x - transform.position.x < 0.0f)
+        else if (positionToMoveTo.x - transform.position.x < 0.0f) //Or are we instead moving to the left?
         {
 
-            moveX = Mathf.Max((positionToMoveTo.x - transform.position.x) / conversionX + transform.position.x, -speed / conversionX + transform.position.x);
+            moveX = Mathf.Max((positionToMoveTo.x - transform.position.x) / conversionX + transform.position.x, -speed / conversionX + transform.position.x); //Same as above line, only difference is we find the Max instead of Min - Bad Code, am also changing
             
-            if (facingRight)
+            if (facingRight) //This checks if we are facing right, if we are we will flip.
             {
 
                 Flip();
@@ -68,35 +70,35 @@ public class MouseClicking : MonoBehaviour
 
             }
 
-            moving = true;
+            moving = true; //You like to, move it!
         }
-        else if (Mathf.Abs(positionToMoveTo.x - transform.position.x) < 0.01f)
+        else if (Mathf.Abs(positionToMoveTo.x - transform.position.x) < 0.01f) //...Uh I was trying to make the movement clamp off at a certain point, hopefully redundant come the lerping.
         {
 
-            moveX = transform.position.x;
-            moving = false;
+            moveX = transform.position.x; //We move NOWHERE!
+            moving = false; //I can't move it move it :(
         }
-        else
+        else //Or if we are not moving at all we do the following.
         {
 
-            moveX = transform.position.x;
-            moving = false;
+            moveX = transform.position.x; //We move NOWHERE!
+            moving = false; //I can't move it move it :(
 
         }
 
 
-        animator.SetBool("IsMoving", moving);
-        transform.position = new Vector3(moveX, -2.2f, transform.position.z);
+        animator.SetBool("IsMoving", moving); //Sets the animator variable of moving?
+        transform.position = new Vector3(moveX, -2.2f, transform.position.z); //We move along the X axis if movement is detected.
 
     }
 
-    void Flip()
+    void Flip() //Flipping code to flip sprites
     {
 
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        facingRight = !facingRight; //This sets facingRight to be the opposite of what it already is.
+        Vector3 theScale = transform.localScale; //We get our scale from the transform.
+        theScale.x *= -1; //By turning the scale from negative to positive or positive to negative we change what way the anteater is facing.
+        transform.localScale = theScale; //We set the scale to be the newly calculated scale.
 
     }
 
