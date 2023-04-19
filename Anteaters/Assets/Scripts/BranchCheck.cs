dcd292c7585dd46d86eb96ceb4f601ff78f6783f
Aden Walker <60225464+Aden-Walker@users.noreply.github.com>
2023-04-19 13:47:42 +0100

    fixed branch

--- Anteaters/Assets/Scripts/BranchCheck.cs
+++ Anteaters/Assets/Scripts/BranchCheck.cs
@@ -5,11 +5,13 @@ public class BranchCheck : MonoBehaviour
 {
     public Inventory inventory;
     public GameObject path;
+    public GameObject branch;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.name == "Player" && inventory.GetBranch())
         {
-            path.SetActive(true);
+            path.layer = 0;
+            branch.SetActive(true);
         }
     }
 }
