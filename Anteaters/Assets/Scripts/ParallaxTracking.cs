cc7d9fd082be6834ef70fc5b82743f38a5a13da3
1901150 <1901150@uad.ac.uk>
2023-05-02 15:04:44 +0100

    Update ParallaxTracking.cs

--- Anteaters/Assets/Scripts/ParallaxTracking.cs
+++ Anteaters/Assets/Scripts/ParallaxTracking.cs
@@ -8,7 +8,10 @@ public class ParallaxTracking : MonoBehaviour
 
     //variables to store shit. parallaxFactor should be a value between 0 and 1 higher values = less movement of the object
     private float startPos;
+    private float angleRange;
+    private float screenEdge;
     public float parallaxFactor;
+    public float rotationFactor;
     public Transform cam;
     
 
@@ -17,6 +20,10 @@ public class ParallaxTracking : MonoBehaviour
     {
         //store the start position of the object
         startPos = transform.position.x;
+        //initialise object's rotation if the rotation factor is 0 this changes nothing
+        angleRange = 45 * rotationFactor;
+        transform.Rotate(new Vector3(0, 0, -angleRange));
+        screenEdge = cam.GetComponent<CameraTracking>().cameraEdgeX;
     }
 
     // Update is called once per frame
@@ -26,6 +33,13 @@ public class ParallaxTracking : MonoBehaviour
         float distance = cam.position.x * parallaxFactor;
         Vector3 newPosition = new Vector3(startPos + distance, transform.position.y, transform.position.z);
         transform.position = newPosition;
-     
+        if (newPosition.x > 0)
+        {
+            float screenFraction = newPosition.x / screenEdge;
+            if(screenFraction < 0.5)
+            {
+
+            }
+        }
     }
 }
